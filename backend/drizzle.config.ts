9dd4256a407ff9e723a8e8abf49e177e6e5eb617
Felipe Coimbra <felipegabycoimbra@hotmail.com>
2024-05-29 18:19:57 -0300

    prototipo alfa do alfa das rotas iniciais
    preciso configurar o drizzle pra usar o postgresql. talvez criar "rotinhas" pra fazer operacoes basicas seja o ideal.

--- backend/drizzle.config.ts
+++ backend/drizzle.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from "drizzle-kit";
 
 export default defineConfig({
-  schema: "./src/drizzle/schema.ts",
+  schema: "./src/drizzle/*",
   dialect: "postgresql",
   dbCredentials: {
     url: (process.env.DB_URL as string) || "random-url",
